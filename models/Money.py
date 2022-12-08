255aee5ca4b84acaffaaaa40c4364da9421e4e57
cozy-hn <110678456+cozy-hn@users.noreply.github.com>
2022-12-09 02:20:18 +0900

    player 수정 예시 재 조정

--- models/Money.py
+++ models/Money.py
@@ -1,27 +1,17 @@
 class Money:
     def __init__(self,money:int =0) -> None:
         self.money=money
-        self.betsum=0
-        self.callfee=10000
-        self.betfee=0
-        
+
     def getmoney(self)->int:
+        return self.money    
+    
+    def setmoney(self,money)->None:
+        self.money=money
+        
+    def plus(self,money:int)->int:
+        self.money+=money
         return self.money
     
-    def getbetsum(self)->int:
-        return self.betsum
-    
-    def getcallfee(self)->int:
-        return self.callfee
-    
-    def getbetfee(self)->int:
-        return self.betfee
-    
-    # def setmoney(self,money:int=0)->None:
-    #     self.money=money
-    
-    def bet(self,bet:int)->None:
-        self.money-=bet
-        
-    def playerbet(self,playerbet:int)->None: #판에 깔린돈 객체 용
-        self.money+=playerbet
+    def minus(self,money:int)->int: #판에 깔린돈 객체 용
+        self.money-=money
+        return self.money
--- models/Player.py
+++ models/Player.py
@@ -2,39 +2,62 @@
 # 컴퓨터 플레이어 클래스는 플레이어 클래스로부터 파생됩니다.
 from .Money import Money
 from .Hand import Hand
-class player(Money,Hand):
-       
-    def __init__(self)->None:
-        Money.__init__(self)
-        Hand.__init__(self)
-        self.turn:int=0
+
+class Player:     
+    def __init__(self,money=Money(500000))->None:
+        self.money: Money =money
+        self.hands: [Hand] = []
         self.alive:bool=True
-    def setturn(self,turn:int):
+        self.callfee:int=0 #콜비용
+        self.betsum:int=0 #배팅한 돈의 총액
+        self.betfee:int=0 #자신의 턴에 배팅 할 돈
+        
+    def setturn(self,turn:int)->None:
         self.turn:int=turn
-    def getturn(self):
+        
+    def getturn(self)->int:
         return self.turn
-    def survive(self): #getalive()
-        return self.alive
     
+    def survive(self)->bool: #getalive()
+        return self.alive   #이걸 여기서 처리할까요?
+    
+    def getbetsum(self)->int:
+        return self.betsum
+    
+    def getbetfee(self)->int:
+        return self.betfee
+    
+    def get_hands(self) -> list:
+        return self.hands
+    
+    def showcallbetfee(self,otherplayerbetsum:int)->int:
+        return abs(self.betsum-otherplayerbetsum)
+    
+    def showhalfbetfee(self,otherplayerbet:int,moneyonthegame:int)->int:
+        return abs(self.betsum-otherplayerbet)+(abs(self.betsum-otherplayerbet)+moneyonthegame)//2
+    
+    def bet(self,money):
+        self.money.minus(money)
+        self.betsum+=money
     # def die(self):
     #     self.alive=False
         
-    def call(self,otherplayerbet:int):
-        self.callfee=abs(self.betsum-otherplayerbet)
+    def call(self,otherplayerbetsum:int)->None:
+        self.callfee=abs(self.betsum-otherplayerbetsum)
         self.betfee=self.callfee
-        if self.betfee>self.money:
+        if self.betfee>self.money.getmoney():
             self.alive=False
         self.betsum+=self.betfee
-        self.money-=self.betfee
+        self.money.minus(self.betfee)
         
-    def half(self,otherplayerbet:int,moneyonthegame:int):
+    def half(self,otherplayerbet:int,moneyonthegame:int)->None:
         self.callfee=abs(self.betsum-otherplayerbet)
-        self.betfee=self.callfee+(self.callfee+moneyonthegame)/2
-        if self.betfee>self.money:
+        self.betfee=self.callfee+(self.callfee+moneyonthegame)//2
+        if self.betfee>self.money.getmoney():
             self.alive=False
         self.betsum+=self.betfee
-        self.money-=self.betfee
-        
+        self.money.minus(self.betfee)
+    
     # def quarter(self,otherplayerbet,moneyonthegame):
     #     self.callfee=abs(self.betsum-otherplayerbet)
     #     self.betfee=self.callfee+(self.callfee+moneyonthegame)/4
